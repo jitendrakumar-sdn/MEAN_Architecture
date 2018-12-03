480db5bd5f74b79f159f79ba1399d66d3d781cb1
jitendrakumar-sdn <jitendraku@smartdatainc.net>
2018-12-03 16:27:44 +0530

    Commit

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(5171);
+  await app.listen(3000);
 }
 bootstrap();
 
